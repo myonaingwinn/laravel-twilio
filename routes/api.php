5478b7f2ad23e9f6f7de7afe196893eac836a6da
myonaingwinn <myonaingwin.gic@gmail.com>
2022-06-07 15:13:51 +0630

    add API route for RoomController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\v1\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::get('/test', function () {
+        return "OK";
+    });
+
+    Route::post('token', [RoomController::class, 'getToken']);
 });
