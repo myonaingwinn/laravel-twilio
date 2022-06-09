d800723e3caae42ef5e9d23a3ed6f56384e1b44c
myonaingwinn <myonaingwin.gic@gmail.com>
2022-06-09 09:36:22 +0630

    JoinRoom component updated

---
+++ frontend/src/components/VideoConference/JoinRoom.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+const JoinRoom = ({
+    username,
+    handleUsernameChange,
+    roomName,
+    handleRoomNameChange,
+    handleSubmit,
+    connecting,
+}) => {
+    return (
+        <Row>
+            <Col></Col>
+            <Col lg={6}>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3">
+                        <h2>Create or join a room</h2>
+                        <Form.Label htmlFor="name">Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="field"
+                            value={username}
+                            onChange={handleUsernameChange}
+                            readOnly={connecting}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="room">Room name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="room"
+                            value={roomName}
+                            onChange={handleRoomNameChange}
+                            readOnly={connecting}
+                            required
+                        />
+                    </Form.Group>
+                    <Button
+                        type="submit"
+                        className="btn btn-primary"
+                        disabled={connecting}
+                    >
+                        {connecting ? "Connecting" : "Join"}
+                    </Button>
+                </Form>
+            </Col>
+            <Col></Col>
+        </Row>
+    );
+};
+
+export default JoinRoom;
