f592bc104990edd1b4f89e7475993f92d3a179e4
Theint-24 <theinttheinthmawe24@gmail.com>
2022-06-10 13:52:43 +0630

    Update Register and Login

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,30 +1,68 @@
-import React, { Component } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
-export default class Login extends Component {
-    render() {
+export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = useCallback(async (event) => {
+        event.preventDefault();
+        const data = await fetch("http://127.0.0.1:8000/api/v1/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => {
+            console.log(res);
+            return res;
+        });
+    });
+
+    const handleEmailChange = useCallback((event) => {
+        setEmail(event.target.value);
+    }, []);
+
+    const handlePasswordChange = useCallback((event) => {
+        setPassword(event.target.value);
+    }, []);
+
+    
         return ( 
             <Form>
-               <h3>Login</h3>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+            <Form.Group className="mb-3">
+            <h2>Login</h2>
+                <Form.Label htmlFor="email">Email:</Form.Label>
+                <Form.Control
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="pwd">Password:</Form.Label>
+                <Form.Control
+                    type="password"
+                    id="pwd"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                />
+            </Form.Group>
+            <Button
+            onClick={handleSubmit}
+                type="submit"
+                className="btn btn-primary"
+            >
+                Login
+            </Button>
+        </Form>
         );
-    }
-}
+};
+
+export default Login;
--- frontend/src/components/Login/Register.js
+++ frontend/src/components/Login/Register.js
@@ -1,39 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
-export default class SignUp extends Component {
-  render() {
+export const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = useCallback(async (event) => {
+        event.preventDefault();
+        const data = await fetch("http://127.0.0.1:8000/api/v1/register", {
+            method: "POST",
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => {
+            console.log(res);
+            return res;
+        });
+    });
+
+    const handleNameChange = useCallback((event) => {
+        setName(event.target.value);
+    }, []);
+
+    const handleEmailChange = useCallback((event) => {
+        setEmail(event.target.value);
+    }, []);
+
+    const handlePasswordChange = useCallback((event) => {
+        setPassword(event.target.value);
+    }, []);
+
     return (
-      <form>
-        <h3>Register</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input type="text" className="form-control" placeholder="First name"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input type="email" className="form-control" placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input type="password" className="form-control" placeholder="Enter password"
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Register
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/login">Login</a>
-        </p>
-      </form>
-    )
-  }
-}
+        <Form>
+            <Form.Group className="mb-3">
+                <h2>Register</h2>
+                <Form.Label htmlFor="name">Name:</Form.Label>
+                <Form.Control
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={handleNameChange}
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="email">Email:</Form.Label>
+                <Form.Control
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="pwd">Password:</Form.Label>
+                <Form.Control
+                    type="password"
+                    id="pwd"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                />
+            </Form.Group>
+            <Button
+                onClick={handleSubmit}
+                type="submit"
+                className="btn btn-primary"
+            >
+                Register
+            </Button>
+        </Form>
+    );
+};
+export default SignUp;
